90aca0be199143a5061cbb5bdd996430fd3e808d
NguyenPTN <nguyenptn1104@gmail.com>
2024-10-18 16:51:35 +0700

    Format source code

--- SpacePortal/ViewModels/InformationViewModel.cs
+++ SpacePortal/ViewModels/InformationViewModel.cs
@@ -6,6 +6,13 @@ using SpacePortal.Core.Contracts;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml;
 using System.Globalization;
+using Microsoft.UI.Xaml.Media.Imaging;
+using Windows.Storage.Pickers;
+using Windows.Storage;
+using WinRT.Interop;
+using System.Net;
+using Microsoft.Windows.ApplicationModel.Resources;
+using System.Text.RegularExpressions;
 namespace SpacePortal.ViewModels;
 
 public partial class InformationViewModel : ObservableRecipient
@@ -45,8 +52,30 @@ public partial class InformationViewModel : ObservableRecipient
         informations.Address = EditAddress;
         IsEditing = false;
     }
-    
 
+    public string CheckAcceptChanges()
+    {
+        ResourceLoader resourceLoader = new ResourceLoader();
+
+        if (string.IsNullOrWhiteSpace(EditPersonalEmail) || string.IsNullOrWhiteSpace(EditPhoneNumber) || string.IsNullOrWhiteSpace(EditAddress))
+        {
+            return resourceLoader.GetString("Information_Error_NotEmpty/Text");
+        }
+
+        string emailPattern = @"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.(com|org|net|edu)$";
+        if (!Regex.IsMatch(EditPersonalEmail, emailPattern))
+        {
+            return resourceLoader.GetString("Information_Error_Email/Text");
+        }
+
+        if (EditPhoneNumber.Length != 10 || !EditPhoneNumber.All(char.IsDigit))
+        {
+            return resourceLoader.GetString("Information_Error_PhoneNumber/Text");
+        }
+
+        return "Success";
+    }
+        
     public void CancelChanges()
     {
         EditPersonalEmail = informations.PersonalEmail;
@@ -55,5 +84,44 @@ public partial class InformationViewModel : ObservableRecipient
         IsEditing = false;
     }
 
+    public void ChangeContact()
+    {
+        if (IsEditing)
+        {
+            EditPersonalEmail = informations.PersonalEmail;
+            EditPhoneNumber = informations.PhoneNumber;
+            EditAddress = informations.Address;
+        }
+        IsEditing = !IsEditing;
+    }
 
+    public async void UploadAvatar()
+    {
+        FileOpenPicker fileOpenPicker = new()
+        {
+            ViewMode = PickerViewMode.Thumbnail,
+            FileTypeFilter = { ".jpg", ".jpeg", ".png", ".gif" },
+        };
+
+        nint windowHandle = WindowNative.GetWindowHandle(App.MainWindow);
+        InitializeWithWindow.Initialize(fileOpenPicker, windowHandle);
+
+        StorageFile file = await fileOpenPicker.PickSingleFileAsync();
+
+        if (file != null)
+        {
+
+            BitmapImage avatarImage = new BitmapImage(new Uri(file.Path));
+            informations.AvatarUrl = avatarImage;
+        }
+        else
+        {
+            //TODO
+        }
+    }
+
+    public void RemoveAvatar()
+    {
+        informations.SetAvatarUrl("ms-appx:///Assets/defaultAvt.png");
+    }
 }
