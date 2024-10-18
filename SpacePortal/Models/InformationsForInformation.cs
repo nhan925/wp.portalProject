163b9a92225dc05bc46c6cc7b3b4eda131bba89b
NguyenPTN <nguyenptn1104@gmail.com>
2024-10-18 09:41:23 +0700

    Binding data and Update edit avatar box

---
+++ SpacePortal/Models/InformationsForInformation.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpacePortal.Models
+{
+    public class InformationsForInformation : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public string FullName
+        {
+            get; set;
+        }
+        public string Major
+        {
+            get; set;
+        }
+        public string AcademicProgram
+        {
+            get; set;
+        }
+        public string StudentID
+        {
+            get; set;
+        }
+        public int YearOfAdmission
+        {
+            get; set;
+        }
+        public string Gender
+        {
+            get; set;
+        }
+        public DateTime DateOfBirth
+        {
+            get; set;
+        }
+        public string IdentityCardNumber
+        {
+            get; set;
+        }
+        public string Nationality
+        {
+            get; set;
+        }
+        public string Ethnicity
+        {
+            get; set;
+        }
+        public string SchoolEmail
+        {
+            get; set;
+        }
+        public string Address
+        {
+            get; set;
+        }
+        public string PhoneNumber
+        {
+            get; set;
+        }
+        public string PersonalEmail
+        {
+            get; set;
+        }
+        public string AvatarUrl
+        {
+            get; set;
+        }
+
+    }
+}
--- SpacePortal/Views/InformationPage.xaml.cs
+++ SpacePortal/Views/InformationPage.xaml.cs
@@ -16,4 +16,25 @@ public sealed partial class InformationPage : Page
         ViewModel = App.GetService<InformationViewModel>();
         InitializeComponent();
     }
+
+    private void EditAvatar_Click(object sender, Microsoft.UI.Xaml.RoutedEventArgs e)
+    {
+        ///TODO
+    }
+
+    private void RemoveAvatar_Click(object sender, Microsoft.UI.Xaml.RoutedEventArgs e)
+    {
+        //TODO
+    }
+
+    private void ChangeAvatar_Click(object sender, Microsoft.UI.Xaml.RoutedEventArgs e)
+    {
+        //TODO
+
+    }
+
+    private void ChangeContact_Click(object sender, Microsoft.UI.Xaml.RoutedEventArgs e)
+    {
+        //TODO
+    }
 }
