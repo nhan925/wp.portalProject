95e8ee0e353a0b10442f7a21589912936b74c8df
NGUYEN MINH NGUYEN <nguyenptn1104@gmail.com>
2024-10-20 11:32:13 +0700

    Update Re-Examination Form (UI and Funtion)

--- SpacePortal/Views/GradesPage.xaml.cs
+++ SpacePortal/Views/GradesPage.xaml.cs
@@ -6,11 +6,14 @@ using SpacePortal.Core.DataAccess;
 using SpacePortal.Core.Models;
 using SpacePortal.ViewModels;
 using Windows.ApplicationModel.VoiceCommands;
+using Microsoft.Windows.ApplicationModel.Resources;
 
 namespace SpacePortal.Views;
 
 public sealed partial class GradesPage : Page
 {
+
+    private readonly ResourceLoader resourceLoader = new();
 
     public GradesViewModel ViewModel
     {
@@ -50,5 +53,33 @@ public sealed partial class GradesPage : Page
     private void CalculatorButton_Click(object sender, RoutedEventArgs e)
     {
        
+    }
+
+    private void MenuFlyoutItem_Click(object sender, RoutedEventArgs e)
+    {
+
+    }
+
+    private async void RequestReview_Click(object sender, RoutedEventArgs e)
+    {
+        var dialog = new ContentDialog();
+        dialog.XamlRoot = this.XamlRoot;
+        dialog.Title = resourceLoader.GetString("GradesPage_RequestForReviewTitle");
+        dialog.Content = new RequestReExaminationDialog(ViewModel.SourceData);
+        dialog.HorizontalAlignment = HorizontalAlignment.Left;
+        
+        
+        dialog.PrimaryButtonText = resourceLoader.GetString("Dialog_Send");
+        dialog.CloseButtonText = resourceLoader.GetString("Dialog_Cancel");
+        dialog.PrimaryButtonStyle = Application.Current.Resources["AccentButtonStyle"] as Style;
+        dialog.Style = Application.Current.Resources["DefaultContentDialogStyle"] as Style;
+        dialog.PrimaryButtonClick += (s, e) =>
+        {
+            var requestReExaminationDialog = dialog.Content as RequestReExaminationDialog;
+            requestReExaminationDialog.SendRequest();
+        };
+
+
+        await dialog.ShowAsync();
     }
 }
---
+++ SpacePortal/Views/RequestReExaminationDialog.xaml.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using SpacePortal.Core.Models;
+using SpacePortal.ViewModels;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace SpacePortal.Views;
+/// <summary>
+/// An empty page that can be used on its own or navigated to within a Frame.
+/// </summary>
+public sealed partial class RequestReExaminationDialog : Page
+{
+   
+    public RequestReExaminationDialogViewModel ViewModel
+    {
+        get;
+    }
+
+    public RequestReExaminationDialog(ObservableCollection<InformationsForGradesPage_GradesRow> sourceData)
+    {
+        this.InitializeComponent();
+        ViewModel = new RequestReExaminationDialogViewModel(sourceData);
+    }
+
+    public void SendRequest()
+    {
+        ViewModel.SendRequest();
+    }
+
+    private void YearComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+    {
+
+        if (YearComboBox.SelectedItem == null)
+        {
+            SemesterComboBox.IsEnabled = false;
+            ViewModel.SelectedSemester = null;
+        }
+        else
+        {
+            SemesterComboBox.IsEnabled = true;
+        }
+
+        if (YearComboBox.SelectedItem != null)
+        {
+            ViewModel.SelectedSemester = null;
+        }
+    }
+}
