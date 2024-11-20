63edb8b5f2d60d15a60197493d91ee0d5703ce37
MaThanhNhi <mathanhnhi2004@gmail.com>
2024-11-20 21:38:21 +0700

    Implement App Feedback and update UI

--- SpacePortal/Views/AppFeedbackPage.xaml.cs
+++ SpacePortal/Views/AppFeedbackPage.xaml.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.UI.Xaml;
+﻿using System.Xml.Serialization;
+using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
+using Microsoft.Windows.ApplicationModel.Resources;
 
 using SpacePortal.ViewModels;
 
@@ -16,15 +18,49 @@ public sealed partial class AppFeedbackPage : Page
     {
         ViewModel = App.GetService<AppFeedbackViewModel>();
         InitializeComponent();
+        setupDefaultComboBox();
+    }
+
+    private void setupDefaultComboBox()
+    {
+        FeedbackClassificationComboBox.SelectedIndex = 0;
     }
 
     private void FeedbackClassificationComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
     {
-        // Do something
+        ViewModel.changeType();
     }
 
     private void SendButton_Click(object sender, RoutedEventArgs e)
     {
-        // Do something
+        ResourceLoader resourceLoader = new ResourceLoader();
+        string message = ViewModel.checkFeedback();
+        string successMessage = resourceLoader.GetString("AppFeedback_SuccessMessageDialog/Text");
+        string title = "";
+
+        if (message == successMessage)
+        {
+            ViewModel.sendFeedback();
+            ViewModel.EditContent = "";
+            title = resourceLoader.GetString("AppFeedback_SuccessTitleDialog/Text");
+        }
+        else
+        {
+            title = resourceLoader.GetString("App_Error/Text");
+        }
+        ShowMessage(message, title);
+    }
+
+    private async void ShowMessage(string message, string title)
+    {
+        ContentDialog errorDialog = new ContentDialog
+        {
+            Title = title,
+            Content = message,
+            CloseButtonText = new ResourceLoader().GetString("App_Close/Text"),
+            XamlRoot = this.Content.XamlRoot
+        };
+
+        await errorDialog.ShowAsync();
     }
 }
