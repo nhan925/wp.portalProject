d6db655b93f97f956c0d157af1349204c34f5616
Nguyen Trong Nhan <info.nhan925@gmail.com>
2024-11-27 23:56:01 +0700

    fix title of request

--- SpacePortal/Models/InformationsForRequest_RequestRow.cs
+++ SpacePortal/Models/InformationsForRequest_RequestRow.cs
@@ -36,26 +36,9 @@ public class InformationsForRequest_RequestRow : INotifyPropertyChanged
     public string RequestName
     {
         get
-        {
-            ResourceLoader resourceLoader = new ResourceLoader();
-            var requestReExamiantion = resourceLoader.GetString("GradesPage_RequestForReviewTitle");
-            var requestPhysicalTranscript = resourceLoader.GetString("GradesPage_RequestForTranscriptTitle");
-
-            var requestName = "";
-                
-            var lines = Content.Split("\r\n");
-            if (lines[0] == requestReExamiantion)
-            {
-                var semesterString = resourceLoader.GetString("GradesPage_RequestForReviewSemester/Text");
-                requestName = $"{lines[0]} {semesterString} {lines[2].Split(": ")[1]} - {lines[1].Replace($": ", $" ").Trim()}";
-            }
-            else if (lines[0] == requestPhysicalTranscript)
-            {
-                var totalTranscriptString = resourceLoader.GetString("GradesPage_RequestForTranscriptTotal/Text");
-                var numberOfTranscript = lines[lines.Length - 1].Split(": ")[1];
-                requestName = $"{lines[0]} - {totalTranscriptString}: {numberOfTranscript}";
-            }
-            return requestName;
+        {   
+           var lines = Content.Split("\r\n");
+            return lines[0];
         }
     }
 
