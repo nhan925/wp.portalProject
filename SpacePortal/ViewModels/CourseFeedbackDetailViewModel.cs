a4c7780d57e46d42bd0ab8516cc213bcfc4b4a18
NGUYEN MINH NGUYEN <nguyenptn1104@gmail.com>
2024-12-16 14:55:57 +0700

    Add function and API for course feedback

--- SpacePortal/ViewModels/CourseFeedbackDetailViewModel.cs
+++ SpacePortal/ViewModels/CourseFeedbackDetailViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.ObjectModel;
 using CommunityToolkit.Mvvm.ComponentModel;
+using SpacePortal.Core.Services;
 using SpacePortal.Models;
 using Windows.ApplicationModel.Resources;
 namespace SpacePortal.ViewModels;
@@ -8,7 +9,7 @@ public partial class CourseFeedbackDetailViewModel : ObservableRecipient
 {
     public CourseFeedbackDetailInformations Informations { get; set; } =  new CourseFeedbackDetailInformations();
 
-    public ObservableCollection<Question> CourseQuesntion { get; set; } = new ObservableCollection<Question>();
+    public ObservableCollection<Question> CourseQuestion { get; set; } = new ObservableCollection<Question>();
 
     public ObservableCollection<Question> TeacherQuestion { get; set; } = new ObservableCollection<Question>();
 
@@ -21,23 +22,76 @@ public partial class CourseFeedbackDetailViewModel : ObservableRecipient
         Informations.classID = classID;
     }
 
-    public void LoadCourseQuesntion()
+    public bool UploadFeedbackToDatabase()
+    {
+        var message = CreateFeedbackMessage();
+        var info = new
+        {
+            v_class_id = Informations.classID,
+            v_feedback = message
+        };
+        var result = App.GetService<ApiService>().Post<string>("/rpc/add_feedback_course",info);
+        if(result == "Success")
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public bool CheckEmptyField()
+    {
+        foreach (var question in CourseQuestion)
+        {
+            if (question.SelectedRating == 0)
+            {
+                return false;
+            }
+        }
+
+        foreach (var question in TeacherQuestion)
+        {
+            if (question.SelectedRating == 0)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public string CreateFeedbackMessage()
+    {
+        var courseFeedbackMessage = "Đánh giá môn học: ";
+        var teacherFeedbackMessage = "Đánh giá giảng viên: ";
+        for (var i = 0; i < 5; i++)
+        {
+            courseFeedbackMessage += $"Câu hỏi: {i +1} - {CourseQuestion[i].SelectedRating} ";
+            teacherFeedbackMessage += $"Câu hỏi: {i + 1} - {TeacherQuestion[i].SelectedRating} ";
+        }
+
+        var fullFeedbackMessage = courseFeedbackMessage + " | " + teacherFeedbackMessage;
+        return fullFeedbackMessage;
+    }
+
+    public void LoadCourseQuestion()
     {
         ResourceLoader resourceLoader = new();
         for (var i = 1; i <= 5 ; i++)
         {
             var questionContent = resourceLoader.GetString($"CourseFeedbackDetail_CourseQuestion0{i}/Text");
-            CourseQuesntion.Add(new Question { Number = i, Content = questionContent });
+            CourseQuestion.Add(new Question { Number = i, Content = questionContent });
         }
     }
 
-    public void LoadTeacherQuesntion()
+    public void LoadTeacherQuestion()
     {
         ResourceLoader resourceLoader = new();
         for (var i = 1; i <= 5; i++)
         {
             var questionContent = resourceLoader.GetString($"CourseFeedbackDetail_TeacherQuestion0{i}/Text");
-            TeacherQuestion.Add(new Question { Number = i, Content = questionContent });
+            TeacherQuestion.Add(new Question { Number = i + 5, Content = questionContent });
         }
     }
 }
--- SpacePortal/Views/CourseFeedbackDetailPage.xaml.cs
+++ SpacePortal/Views/CourseFeedbackDetailPage.xaml.cs
@@ -2,21 +2,21 @@
 using Microsoft.UI.Xaml.Navigation;
 using SpacePortal.Contracts.Services;
 using SpacePortal.ViewModels;
-
+using WinUIEx.Messaging;
+using Microsoft.Windows.ApplicationModel.Resources;
+using System.Diagnostics;
 namespace SpacePortal.Views;
 
 public sealed partial class CourseFeedbackDetailPage : Page
 {
-    public CourseFeedbackDetailViewModel ViewModel
-    {
-        get;
-    }
+    public CourseFeedbackDetailViewModel ViewModel { get; }
 
     public CourseFeedbackDetailPage()
     {
         ViewModel = App.GetService<CourseFeedbackDetailViewModel>();
         InitializeComponent();
     }
+
     protected async override void OnNavigatedTo(NavigationEventArgs e)
     {
         base.OnNavigatedTo(e);
@@ -26,8 +26,8 @@ public sealed partial class CourseFeedbackDetailPage : Page
             ViewModel.LoadInformations(info.Item1, info.Item2, info.Item3, info.Item4);
         }
 
-        ViewModel.LoadCourseQuesntion();
-        ViewModel.LoadTeacherQuesntion();
+        ViewModel.LoadCourseQuestion();
+        ViewModel.LoadTeacherQuestion();
         //CourseFeeListGrid.Visibility = Visibility.Visible;
         //CourseFeeListLoading.Visibility = Visibility.Collapsed;
     }
@@ -38,8 +38,46 @@ public sealed partial class CourseFeedbackDetailPage : Page
         navigationService.GoBack();
     }
 
-    private void Complete_Click(object sender, Microsoft.UI.Xaml.RoutedEventArgs e)
+    private async void Complete_Click(object sender, Microsoft.UI.Xaml.RoutedEventArgs e)
     {
-        //TODO
+        var result = ViewModel.CheckEmptyField();
+
+        if (!result)
+        {
+            ResourceLoader resourceLoader = new ResourceLoader();
+            ContentDialog errorDialog = new ContentDialog
+            {
+                Title = resourceLoader.GetString("App_Error/Text"),
+                Content = resourceLoader.GetString("CourseFeedbackDetail_EmptyFieldError/Text"),
+                CloseButtonText = resourceLoader.GetString("App_Close/Text"),
+                XamlRoot = this.Content.XamlRoot,
+                RequestedTheme = App.GetService<IThemeSelectorService>().Theme
+            };
+
+            await errorDialog.ShowAsync();
+        }
+        else
+        {
+            if (ViewModel.UploadFeedbackToDatabase())
+            {
+                ResourceLoader resourceLoader = new ResourceLoader();
+                ContentDialog completeDialog = new ContentDialog
+                {
+                    Title = resourceLoader.GetString("App_Success/Text"),
+                    Content = resourceLoader.GetString("CourseFeedbackDetail_CompleteMessage/Text"),
+                    CloseButtonText = resourceLoader.GetString("App_Yes/Text"),
+                    XamlRoot = this.Content.XamlRoot,
+                    RequestedTheme = App.GetService<IThemeSelectorService>().Theme
+                };
+                await completeDialog.ShowAsync();
+
+                var navigationService = App.GetService<INavigationService>();
+                navigationService.GoBack();
+            }
+            else
+            {
+                //TODO: Handle error
+            }
+        }
     }
 }
